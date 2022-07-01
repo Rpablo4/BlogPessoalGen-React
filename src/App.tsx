499ad39acc4c4f822b66e0fee560324648b9eb7b
Ruan Pablo <222ruanpablo@gmail.com>
2022-07-01 17:32:49 -0300

    commit inicial, adicionando footer, navbar e home

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Navbar from './components/static/navbar/Navbar';
+import Footer from './components/static/Footer/Footer';
+import Home from './pages/home/Home';
 import './App.css';
+import Login from './pages/login/Login';
+import { Typography } from '@material-ui/core';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+
+        <Route path="/home" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
